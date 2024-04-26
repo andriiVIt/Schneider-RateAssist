2cd80fb2af7f27d2fabf953ce5f04ffa058d0a8f
Andrii Savchenko <109040480+andriiVIt@users.noreply.github.com>
2024-04-27 00:11:29 +0200

    .

--- GUI/Controller/HRController.java
+++ GUI/Controller/HRController.java
@@ -1,4 +1,11 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+
 public class HRController {
+    public void previousPage(ActionEvent actionEvent) {
+    }
+
+    public void nextPage(ActionEvent actionEvent) {
+    }
 }
