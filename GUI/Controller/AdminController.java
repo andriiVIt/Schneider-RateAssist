71b5301dd5989b936010939f18577b0c2301723f
Andrii Savchenko <109040480+andriiVIt@users.noreply.github.com>
2024-04-26 19:28:01 +0200

    .

--- GUI/Controller/AdminController.java
+++ GUI/Controller/AdminController.java
@@ -1,4 +1,11 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+
 public class AdminController {
+    public void previousPage(ActionEvent actionEvent) {
+    }
+
+    public void nextPage(ActionEvent actionEvent) {
+    }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,27 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Objects;
+
+public class Main extends Application {
+    public static void main(String[] args) {launch(args);
+
+    }
 
 
-    public static void main(String[] args) {
-    System.out.println("Hello");
-}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL fxmlUrl = getClass().getResource("GUI/view/AdminWindow.fxml");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(fxmlUrl));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Rate Assist");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
 }
