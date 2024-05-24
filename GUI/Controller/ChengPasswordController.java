b149373c1a97f537e7b04b3cd62babdd6cb2dc1e
Andrii Savchenko <109040480+andriiVIt@users.noreply.github.com>
2024-05-25 01:13:27 +0200

    css added

---
+++ GUI/Controller/ChengPasswordController.java
@@ -0,0 +1,16 @@
+package GUI.Controller;
+
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+
+public class ChengPasswordController {
+    public MFXPasswordField newPasswordField;
+    public MFXTextField userNameField;
+
+    public void saveInformation(ActionEvent actionEvent) {
+    }
+
+    public void cancel(ActionEvent actionEvent) {
+    }
+}
--- GUI/Controller/WorkerController.java
+++ GUI/Controller/WorkerController.java
@@ -3,17 +3,24 @@ package GUI.Controller;
 import BE.*;
 import BLL.RateLogic;
 import GUI.Model.CalculationModel;
+import GUI.Model.CountryModel;
 import GUI.Model.EmployeeModel;
+import GUI.Model.TeamModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
@@ -149,7 +156,20 @@ public class WorkerController implements Initializable {
     }
 
     @FXML
-    public void changePassword(ActionEvent actionEvent) {
-        // Логіка зміни паролю
+    public void changePassword(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GUI/view/ChangPasswordWindow.fxml"));
+        Parent teamsParent = fxmlLoader.load();
+
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL); // Set the window modality
+        stage.setTitle("New Password");
+        stage.setResizable(false); // Make the window not resizable
+        stage.setScene(new Scene(teamsParent));
+
+
+        stage.show();
     }
-}
+    }
+
+
+
